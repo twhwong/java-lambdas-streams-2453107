9c32d35c29744ff22fbd99d6d35c1220ec7ccea4
Tom W <5216397+twhwong@users.noreply.github.com>
2025-07-26 15:17:45 +0200

    functional interface without input params

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int a, int b);
+
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Random;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -14,6 +16,19 @@ public class Main {
       }
     };
     greeting2.sayHello();
+
+    Greeting greeting3 = () -> System.out.println("Hello World");
+    greeting3.sayHello();
+
+    Calculator calculator = (a, b) -> {
+      Random random = new Random();
+      int r = random.nextInt(10);
+      System.out.println("r = " + r);
+      return r + (a * b);
+    };
+
+    System.out.println(calculator.calculate(2, 4));
+
 
   }
 
